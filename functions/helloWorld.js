61076619fea194c85a5eed537a9aadcf772c7b58
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2023-03-31 17:03:34 -0700

    actually use buffer

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,5 +1,7 @@
-import buffer from "node:buffer";
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.from('hello world', 'utf8');
 
 export function onRequest(context) {
-    return new Response("Hello, world!")
+    return new Response(buf.toString('hex'))
   }
